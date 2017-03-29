b3eaa73fbba29cdcf0dafa6739709c36742c4384
Megan Wiley <mwiley322@gmail.com>
2017-03-29 15:03:03 -0700

    Search form functions work to handle updating query state

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      query: ''
+    }
+  }
+  //this will be the function updating our query state
+  updateSearchQuery() {
+    let searchQuery = this.refs.mySearch.value;
+    this.setState({
+      query: searchQuery
+    });
+  }
+//this is where our ajax call to search will go ultimately
+  handleSearchSubmit (e) {
+    e.preventDefault();
+    console.log(this.state.query);
+    this.refs.mySearch.value='';
+  }
   render() {
     return (
-      <form>
-        <input id='searchForm' type='text' name='search' placeholder='Search...' />
+      <form onSubmit={(e)=>this.handleSearchSubmit(e)}>
+        <input onChange={(e)=>this.updateSearchQuery(e)} id='searchForm' ref="mySearch" type='text' placeholder='Search...' />
       </form>
     );
   }
